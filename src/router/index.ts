40c8bdb39bd72c7c7052a9d4db01d84d0b553189
bilgebvli <35532766+bilgebvli@users.noreply.github.com>
2022-11-02 23:04:33 +0300

    Root route was added

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/home',
+    path: '/',
     name: 'home',
     component: () =>
       import(/* webpackChunkName: "about" */ '../views/home/index.vue'),
